041425e20834f57ff7f439ce99719f14d2613399
Mark Diekhans <markd@ucsc.edu>
2021-11-28 10:15:39 -0800

    added mapped exon CDS and frame

--- lib/xspecies/data.py
+++ lib/xspecies/data.py
@@ -1,5 +1,6 @@
 from pycbio.sys.objDict import ObjDict
 from pycbio.hgdata.bed import Bed
+from pycbio.hgdata.frame import Frame
 
 class Range(ObjDict):
     def __init__(self, start, end):
@@ -13,21 +14,37 @@ class Range(ObjDict):
     def overlaps(self, rng):
         return (self.start < rng.end) and (self.end > rng.start)
 
+    def contains(self, rng):
+        return (self.start >= rng.start) and (self.end <= rng.end)
+
+    def intersect(self, rng):
+        return Range(max(self.start, rng.start),
+                     min(self.end, rng.end))
+
 class Coords(Range):
     def __init__(self, chrom, start, end, strand):
         super().__init__(start, end)
         self.chrom = chrom
         self.strand = strand
 
+    def intersect(self, rng):
+        n = super().intersect(rng)
+        n.chrom = self.chrom
+        n.strand = self.strand
+        return n
+
 class MappedExon(ObjDict):
     """exon mapped to another assembly.  This will list all of the INDEL in
     source exon. start/end can be None if exon is not mapped"""
-    def __init__(self, srcExonId, src, srcBases, mapped=None, mappedBases=0):
+    def __init__(self, srcExonId, src, srcBases, *, mapped=None, mappedBases=0,
+                 cds=None, frame=None):
         self.srcExonId = srcExonId
         self.src = src
         self.srcBases = srcBases
         self.mapped = mapped
         self.mappedBases = mappedBases
+        self.cds = cds
+        self.frame = frame
 
     def __str__(self):
         return f"{self.srcExonId} {self.src} => {self.mapped}"
@@ -75,3 +92,27 @@ def mappedTranscriptToBed(trans):
     mt = trans.mapped
     return Bed(mt.chrom, mt.start, mt.end, trans.mappedTransId, score=0, strand=mt.strand,
                thickStart=trans.cds.start, thickEnd=trans.cds.end, itemRgb=0, blocks=blocks)
+
+def _getOverlappedExons(mappedGp, exonRegion):
+    return [e for e in mappedGp.exons
+            if exonRegion.overlaps(e)]
+
+def _getFrame(strand, overExons):
+    # first from start or end with frame
+    if strand == '-':
+        overExons = reversed(overExons)
+    for exon in overExons:
+        if exon.frame >= 0:
+            return Frame(exon.frame)
+    raise Exception(f"frame not found: {overExons}")
+
+def getMappedExonFrame(mappedGp, exonRegion):
+    """exonRange is the mapped range of the source exon, return cdsRange, frame,
+     will all being None if no CDS overlap
+    """
+    mappedCds = Range(mappedGp.cdsStart, mappedGp.cdsEnd)
+    if not exonRegion.overlaps(mappedCds):
+        return (None, None)
+    overExons = _getOverlappedExons(mappedGp, exonRegion)
+    exonCds = mappedCds.intersect(exonRegion)
+    return exonCds, _getFrame(mappedGp.strand, overExons)
